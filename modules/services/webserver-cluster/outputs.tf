ead6d9b17e02da22ea0e48c6ba8cf1a2bd2881a1
asemplinski <alex.semplinski@gmail.com>
2022-11-18 08:19:20 -0700

    Fixed Output Names

--- modules/services/webserver-cluster/outputs.tf
+++ modules/services/webserver-cluster/outputs.tf
@@ -2,3 +2,9 @@ output "autoscaling_group_name" {
   description = "DNS name of the Load Balance"
   value = aws_autoscaling_group.example.name
 }
+
+output "alb_dns_name" {
+  description = "DNS name of the Application Load Balancer"
+  value  = aws_lb.example.dns_name
+  
+}
--- prod/services/webserver-cluster/output.tf
+++ prod/services/webserver-cluster/output.tf
@@ -1,7 +1,9 @@
-
-
-
 output "autoscaling_group_name" {
-  description = "DNS name of the Application Load Balancer"
+  description = "Name of the Autoscaling Group"
   value = module.webserver_cluster.autoscaling_group_name
+}
+
+output "alb_dns_name" {
+  description = "DNS Name of the application Load Balancer"
+  value = module.webserver_cluster.alb_dns_name
 }
